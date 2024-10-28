b662113a9f7dbf6db263ada1ae0727a001dd711c
Varun <varunjethani@outlook.com>
2024-10-28 18:30:24 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,159 @@
+import dotenv from 'dotenv';
+import { createLeaderboardByContestId, createLeaderboardByRatings, createSemesterWiseLeaderboards, loadHandleMappingFromDB } from './leaderboards.js';
+
+dotenv.config();
+
+import {
+    Client, 
+    GatewayIntentBits,
+    ButtonBuilder,
+    ButtonStyle,
+    SlashCommandBuilder,
+    ActionRowBuilder, 
+    Events, 
+    ModalBuilder,
+    TextInputBuilder, TextInputStyle 
+  } from 'discord.js';
+
+const client = new Client({
+    intents: [
+        GatewayIntentBits.Guilds,
+        GatewayIntentBits.GuildMessages,
+        GatewayIntentBits.MessageContent
+    ]
+})
+
+
+const ratingbtn = new ButtonBuilder().setCustomId('ratings').setLabel('Ratings').setStyle(ButtonStyle.Primary)
+    
+
+const cntstbtn = new ButtonBuilder().setCustomId('contests').setLabel('Contests').setStyle(ButtonStyle.Primary)
+    
+
+const yearwisebtn = new ButtonBuilder().setCustomId('yearwise').setLabel('Yearwise').setStyle(ButtonStyle.Primary)
+
+
+const row = new ActionRowBuilder().addComponents(ratingbtn, cntstbtn, yearwisebtn)
+
+
+
+
+client.login(process.env.DISCORD_TOKEN);
+
+client.on('ready', () => {
+    console.log('Bot is ready');
+})
+
+client.on('messageCreate', (message) => {
+    
+    // else{
+    if (message.author.bot) return;
+    else if (message.mentions.has(client.user)) {
+        if (message.content.startsWith('!leaderboard')) {
+            message.reply({
+                content: 'Choose the type of leaderboard',
+                components: [
+                    new ActionRowBuilder().addComponents(ratingbtn, cntstbtn, yearwisebtn)
+                ]
+            })
+        }
+        else{
+            message.reply({
+                content: 'Hello There :wave: ' ,
+            });
+        }
+    }
+})
+
+client.on(Events.InteractionCreate, async (interaction) => {
+    if (!interaction.isButton()) return;
+
+    if (interaction.customId === 'ratings') {
+        const leaderboard = await createLeaderboardByRatings();
+
+        let Top10 = [];
+        if (leaderboard.length < 11 && leaderboard.length > 0) {
+            Top10 = leaderboard.slice(0, leaderboard.length);
+        }
+        else Top10 = leaderboard.slice(1, 11);
+        const guild = await client.guilds.fetch(interaction.guildId);
+        let message = `LeaderBoard For Rating is\n`;
+        for (let i=0; i<Top10.length; i++){
+            const member = guild.members.cache.find(user => user.user.username === Top10[i]['discordUsername']);
+            
+            // const mention = await mentionUserByUsername(Top10[i]['discordUsername']);
+            message += `${i+1}. ${Top10[i]['name']} <${member}> Rating - ${Top10[i]['rating']}\n`;
+        }
+        await interaction.update({ content: message, components: [],  });
+    } else if (interaction.customId === 'contests') {
+        //show modal for contest id
+        const modal = new ModalBuilder()
+			.setCustomId('contestid')
+			.setTitle('Contest ID');
+        
+        const contestIDInput = new TextInputBuilder()
+        .setCustomId('contestid')
+        .setLabel("Enter the Contest ID")
+        // Paragraph means multiple lines of text.
+        .setStyle(TextInputStyle.Short);
+        const firstActionRow = new ActionRowBuilder().addComponents(contestIDInput);
+        modal.addComponents(firstActionRow);
+        await interaction.showModal(modal);
+
+
+    } else if (interaction.customId === 'yearwise') {
+        const leaderboards = await createSemesterWiseLeaderboards();
+        const guild = await client.guilds.fetch(interaction.guildId);
+        let message = 'Yearwise Leaderboard\n';
+        for (const semester in leaderboards) {
+            message += `\nLeaderboard for Sem ${semester}\n`;
+            const semleaderboard = leaderboards[semester];
+            let Top10 = [];
+            if (semleaderboard.length < 11 && semleaderboard.length > 0) {
+                Top10 = semleaderboard.slice(0, semleaderboard.length);
+            }
+            else Top10 = semleaderboard.slice(0, 10);
+            for (let i=0; i<Top10.length; i++){
+                const member = guild.members.cache.find(user => user.user.username === Top10[i]['discordUsername']);
+                message += `${i+1}. ${Top10[i]['name']} <${member}> Rating - ${Top10[i]['rating']}\n`;
+            }
+        }
+        interaction.update({
+            content: message,
+            components:[]
+        })
+    }
+})
+
+client.on(Events.InteractionCreate, async interaction => {
+	if (!interaction.isModalSubmit()) return;
+    const contestID = interaction.fields.getTextInputValue('contestid');
+    const leaderboard = await createLeaderboardByContestId(contestID);
+    let Top10 = [];
+    if (leaderboard.length < 11 && leaderboard.length > 0) {
+        Top10 = leaderboard.slice(0, leaderboard.length);
+    }
+    else Top10 = leaderboard.slice(1, 11);
+    const guild = await client.guilds.fetch(interaction.guildId);
+
+    let message = `LeaderBoard For ${leaderboard[0]['contestName']}\n`;
+    for (let i=0; i<Top10.length; i++){
+        const member = guild.members.cache.find(user => user.user.username === Top10[i]['discordUsername']);
+        
+        // const mention = await mentionUserByUsername(Top10[i]['discordUsername']);
+        message += `${i+1}. ${Top10[i]['name']} <${member}> Rank - ${Top10[i]['rank']}\n`;
+    }
+    await interaction.update({ content: message, components: [],  });
+    
+});
+
+
+client.on('interactionCreate', async (interaction) => {
+    if (!interaction.isCommand()) return;
+
+    if (interaction.commandName === 'leaderboard') {
+        await interaction.reply( { content: 'Choose the type of leaderboard',components: [row] });
+    }
+});
+
+
---
+++ leaderboards.js
@@ -0,0 +1,162 @@
+
+
+// leaderboard.js
+import dotenv from 'dotenv';
+dotenv.config();
+
+import { MongoClient } from 'mongodb';
+import axios from 'axios';
+
+const mongoUrl = process.env.MONGO_URL;
+
+
+
+
+const client = new MongoClient(mongoUrl);
+
+async function loadHandleMappingFromDB(client) {
+    await client.connect();
+    const collection = client.db('DevClub').collection('Members');
+    
+    const data = await collection.find({}).toArray();
+    
+    const handleMapping = {};
+    data.forEach(row => {
+        const codeforcesHandle = row['Codeforces Handle'];
+        handleMapping[codeforcesHandle] = {
+            discordUsername: row['Discord Username'],
+            name: row.Name,
+            semester: row.Semester
+        };
+    });
+    return handleMapping;
+}
+
+
+async function createLeaderboardByContestId(contestId) {
+    const handleMapping = await loadHandleMappingFromDB(client);
+    try {
+        const response = await axios.get(`https://codeforces.com/api/contest.standings`, {
+            params: {
+                contestId,
+                handles: Object.keys(handleMapping).join(';'), // Pass handles directly
+                showUnofficial: true
+            }
+        });
+
+        const standings = response.data.result.rows;
+        const constestName = response.data.result.contest.name;
+
+        const leaderboard = standings.map((entry) => {
+            const handle = entry.party.members[0].handle;
+            const userInfo =  handleMapping[Object.keys(handleMapping).find(key => key.trim().toLowerCase() === handle.trim().toLowerCase())]
+
+
+            return {
+                rank: entry.rank,
+                discordUsername: userInfo.discordUsername || 'Unknown User',
+                handle,
+                name: userInfo.name || 'N/A',
+                semester: userInfo.semester || 'N/A',
+                points: entry.points,
+                penalty: entry.penalty
+            };
+        });
+
+        leaderboard.unshift({ contestName: constestName });
+
+        return leaderboard;
+    } catch (error) {
+        console.error('Error fetching contest standings:', error);
+        throw new Error('Failed to generate leaderboard.');
+    }
+}
+
+
+async function createLeaderboardByRatings() {
+    
+    const handleMapping = await loadHandleMappingFromDB(client);
+    try {
+        const handles = Object.keys(handleMapping).join(';');
+        const response = await axios.get(`https://codeforces.com/api/user.info`, {
+            params: {
+                handles
+            }
+        });
+
+        const userInfoList = response.data.result;
+
+        const leaderboard = userInfoList.map((userInfo) => {
+            const handle = userInfo.handle;
+            const user = handleMapping[Object.keys(handleMapping).find(key => key.trim().toLowerCase() === handle.trim().toLowerCase())]
+
+            
+            return {
+                discordUsername: user.discordUsername || 'Unknown User',
+                handle,
+                name: user.name || 'N/A',
+                semester: user.semester || 'N/A',
+                rating: userInfo.rating || 0,
+                maxRating: userInfo.maxRating || 0
+            };
+        });
+        //remove unrated
+        const newleaderb = leaderboard.filter(user => user.rating > 0);
+
+        // Sort by rating in descending order
+        newleaderb.sort((a, b) => b.rating - a.rating);
+
+        return newleaderb;
+    } catch (error) {
+        console.error('Error fetching user ratings:', error);
+        throw new Error('Failed to generate ratings leaderboard.');
+    }
+}
+
+
+async function createSemesterWiseLeaderboards() {
+    const semesterWiseLeaderboards = {};
+    const leaderboard = await createLeaderboardByRatings();
+    leaderboard.forEach(user => {
+        const semester = user.semester || 'Unknown';
+        
+        if (!semesterWiseLeaderboards[semester]) {
+            semesterWiseLeaderboards[semester] = [];
+        }
+
+        semesterWiseLeaderboards[semester].push({
+            discordUsername: user.discordUsername,
+            handle: user.handle,
+            name: user.name,
+            semester,
+            rating: user.rating,
+            maxRating: user.maxRating
+        });
+    });
+
+    return semesterWiseLeaderboards;
+
+   
+    // // Group users by semester
+    // Object.entries(handleMapping).forEach(([handle, userInfo]) => {
+    //     const semester = userInfo.semester || 'Unknown';
+        
+    //     if (!semesterWiseLeaderboards[semester]) {
+    //         semesterWiseLeaderboards[semester] = [];
+    //     }
+
+    //     semesterWiseLeaderboards[semester].push({
+    //         discordUsername: userInfo.discordUsername || 'Unknown User',
+    //         handle,
+    //         name: userInfo.name || 'N/A',
+    //         semester
+    //     });
+    // });
+}
+
+export {
+    createLeaderboardByContestId,
+    createLeaderboardByRatings,
+    createSemesterWiseLeaderboards,
+    loadHandleMappingFromDB
+}
